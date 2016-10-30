7bf86b34c6653f630d38e0cd60fe4801cd00ea20
Louis Popi <theh2o64@gmail.com>
2016-10-30 20:09:10 +0100

    Revert "Frameworks: Support AOSP service to scan carrier resources"
    This reverts commit 4a00fb5b3a84411755c66a9ec99b80be88f0f5bc.
    
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- core/jni/android_util_AssetManager.cpp
+++ core/jni/android_util_AssetManager.cpp
@@ -19,7 +19,6 @@
 
 #include <android_runtime/android_util_AssetManager.h>
 
-#include <cutils/properties.h>
 #include <inttypes.h>
 #include <linux/capability.h>
 #include <stdio.h>
@@ -27,7 +26,6 @@
 #include <sys/wait.h>
 
 #include <private/android_filesystem_config.h> // for AID_SYSTEM
-#include <private/regionalization/Enviroment.h>
 
 #include "androidfw/Asset.h"
 #include "androidfw/AssetManager.h"
@@ -121,7 +119,7 @@ jint copyValue(JNIEnv* env, jobject outValue, const ResTable* table,
 }
 
 // This is called by zygote (running as user root) as part of preloadResources.
-static void verifySystemIdmaps(const char* overlay_dir)
+static void verifySystemIdmaps()
 {
     pid_t pid;
     char system_id[10];
@@ -165,7 +163,7 @@ static void verifySystemIdmaps(const char* overlay_dir)
                 }
 
                 execl(AssetManager::IDMAP_BIN, AssetManager::IDMAP_BIN, "--scan",
-                        overlay_dir, AssetManager::TARGET_PACKAGE_NAME,
+                        AssetManager::OVERLAY_DIR, AssetManager::TARGET_PACKAGE_NAME,
                         AssetManager::TARGET_APK_PATH, AssetManager::IDMAP_DIR, (char*)NULL);
                 ALOGE("failed to execl for idmap: %s", strerror(errno));
                 exit(1); // should never get here
@@ -2004,20 +2002,7 @@ static jintArray android_content_AssetManager_getStyleAttributes(JNIEnv* env, jo
 static void android_content_AssetManager_init(JNIEnv* env, jobject clazz, jboolean isSystem)
 {
     if (isSystem) {
-        // Load frameworks-res.apk's overlay through regionalization enviroment
-        if (Enviroment::isSupported()) {
-            Enviroment* enviroment = new Enviroment();
-            if (enviroment != NULL) {
-                const char* overlay_dir = enviroment->getOverlayDir();
-                if (overlay_dir != NULL && strcmp(overlay_dir, "") != 0) {
-                    ALOGD("Regionalization - getOverlayDir:%s", overlay_dir);
-                    verifySystemIdmaps(overlay_dir);
-                }
-                delete enviroment;
-            }
-        }
-
-        verifySystemIdmaps(AssetManager::OVERLAY_DIR);
+        verifySystemIdmaps();
     }
     AssetManager* am = new AssetManager();
     if (am == NULL) {
